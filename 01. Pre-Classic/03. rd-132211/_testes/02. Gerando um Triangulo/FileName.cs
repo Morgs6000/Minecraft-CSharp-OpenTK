1b18253edcc4b7537fdd775862699f307de67799
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2024-05-26 08:18:57 -0300

    Update Commit

---
+++ 01. Pre-Classic/03. rd-132211/_testes/02. Gerando um Triangulo/FileName.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace com {
+    internal class FileName : GameWindow {
+        public FileName() 
+            : base(GameWindowSettings.Default, new NativeWindowSettings() {
+                ClientSize = (1024, 768),
+                Title = "Game"
+            }) {
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args) {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnLoad() {
+            base.OnLoad();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args) {
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e) {
+            base.OnFramebufferResize(e);
+        }
+    }
+}
