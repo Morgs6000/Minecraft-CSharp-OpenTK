96b278b74518c899b8fb387f97e994a2705d3a44
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2024-05-28 13:54:50 -0300

    The Block Class Part 1

---
+++ 01. Pre-Classic/00.a/07. The Block Class Part 1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace openTk_Minecraft_Clone_Tutorial_Series {
+    public class Program {
+        // Entry point of the program
+        static void Main(string[] args) {
+            // Creates game object and disposes of it afet leaving the scope
+            using(Game game = new Game(1280, 720)) {
+                // running the game
+                game.Run();
+            }
+        }
+    }
+}
