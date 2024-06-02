c592f991bb67d0aabb39ca034f439ec6001fb1a1
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2024-06-01 21:34:10 -0300

    Gerando um Mundo [Error]

---
+++ 01. Pre-Classic/03. rd-132211/_teste0/09. Gerando Camadas de Blocos/src/Shaders.cs
@@ -0,0 +1,64 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace RubyDung.src {
+    public class Shaders {
+        public int ID;
+
+        public Shaders() {
+            int success;
+            string infoLog;
+
+            // ..:: Vertex Shader ::..
+            string vertexCode = File.ReadAllText("../../../src/Shaders/shader.vert");
+
+            int vertex;
+            vertex = GL.CreateShader(ShaderType.VertexShader);
+
+            GL.ShaderSource(vertex, vertexCode);
+            GL.CompileShader(vertex);
+
+            GL.GetShader(vertex, ShaderParameter.CompileStatus, out success);
+
+            if(success == 0) {
+                infoLog = GL.GetShaderInfoLog(vertex);
+                Console.WriteLine("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" + infoLog);
+            }
+
+            // ..:: Fragment Shader ::..
+            string fragmentCode = File.ReadAllText("../../../src/Shaders/shader.frag");
+
+            int fragment;
+            fragment = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragment, fragmentCode);
+            GL.CompileShader(fragment);
+
+            GL.GetShader(fragment, ShaderParameter.CompileStatus, out success);
+
+            if(success == 0) {
+                infoLog = GL.GetShaderInfoLog(fragment);
+                Console.WriteLine("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" + infoLog);
+            }
+
+            // ..:: Shader Program ::..
+            this.ID = GL.CreateProgram();
+
+            GL.AttachShader(this.ID, vertex);
+            GL.AttachShader(this.ID, fragment);
+            GL.LinkProgram(this.ID);
+
+            GL.GetProgram(this.ID, GetProgramParameterName.LinkStatus, out success);
+
+            if(success == 0) {
+                infoLog = GL.GetProgramInfoLog(this.ID);
+                Console.WriteLine("ERROR::SHADER::PROGRAM::LINKING_FAILED\n" + infoLog);
+            }
+
+            GL.DeleteShader(vertex);
+            GL.DeleteShader(fragment);
+        }
+
+        public void use() {
+            GL.UseProgram(this.ID);
+        }
+    }
+}
