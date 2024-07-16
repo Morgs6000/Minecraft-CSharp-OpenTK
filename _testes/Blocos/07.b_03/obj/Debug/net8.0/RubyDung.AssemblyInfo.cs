63f246399dc2b4ccd8977d6fda41287287cd5cc3
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2024-07-16 12:59:22 -0300

    Commit Update

--- _testes/Blocos/07.b_03/obj/Debug/net8.0/RubyDung.AssemblyInfo.cs
+++ _testes/Blocos/07.b_03/obj/Debug/net8.0/RubyDung.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7db33835a55f973c23d7f36006b1aa8afe3f3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc4d68be5b75da38fc4289c8cd673b297e84100")]
 [assembly: System.Reflection.AssemblyProductAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RubyDung")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
